c3efae2a51d8eaefbe7ac9dcb5abe6837f1bbf92
Ahmet Yusuf Erdoğan <yusuferdogan@bixos.com>
2023-09-13 14:16:05 +0300

    able to assert rslts[0] == msg.sender, by converting msg.sender to string

--- smart-contracts/contracts/Claim.sol
+++ smart-contracts/contracts/Claim.sol
@@ -19,6 +19,7 @@ contract Claim {
     event LogParams(string sender);
     event LogParams(bytes sender);
     event LogParams(address sender);
+    event LogParams(bool isEq);
 
     modifier onlyValidResponse(IOracle.OracleResponse memory response) {
         require(
@@ -35,12 +36,41 @@ contract Claim {
         token = _token;
     }
 
+    function substring(
+        string memory str,
+        uint startIndex,
+        uint endIndex
+    ) public pure returns (string memory) {
+        bytes memory strBytes = bytes(str);
+        bytes memory result = new bytes(endIndex - startIndex);
+        for (uint i = startIndex; i < endIndex; i++) {
+            result[i - startIndex] = strBytes[i];
+        }
+        return string(result);
+    }
+
     function claim(
         IOracle.OracleResponse memory response
     ) external onlyValidResponse(response) {
         //EXTRACT ADDRESS FROM response.rslts[0] AND MINT TO THIS
         //token.mint(msg.sender);
 
+        string memory senderStr = Strings.toHexString(uint160(msg.sender), 20);
+        emit LogParams(senderStr);
+
+        emit LogParams(
+            keccak256(bytes(senderStr)) ==
+                keccak256(
+                    bytes(
+                        string.concat(
+                            "0x",
+                            substring(response.rslts[0], 26, 66)
+                        )
+                    )
+                )
+        );
+
+        emit LogParams(substring(response.rslts[0], 26, 66));
         emit LogParams(response.rslts[0]);
         // bytes memory byt = bytes(response.rslts[0]);
         // bytes memory strBytes = bytes(abi.decode(byt, (string)));
