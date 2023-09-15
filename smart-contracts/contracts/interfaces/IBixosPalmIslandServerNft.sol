c6e23de23fd349d8b1d0bfd7fd48d4f40f36f2b8
Ahmet Yusuf Erdoğan <yusuferdogan@bixos.com>
2023-09-15 17:00:38 +0300

    update interface

--- smart-contracts/contracts/interfaces/IBixosPalmIslandServerNft.sol
+++ smart-contracts/contracts/interfaces/IBixosPalmIslandServerNft.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 interface IBixosPalmIslandServerNft is IERC721 {
-    function mint(address to) external;
+    function lockerMint(address user, uint256 tokenId) external;
+
+    function burn(uint256 tokenId) external;
 }
